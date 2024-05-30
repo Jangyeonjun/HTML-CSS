3b22a61e620738748e16f9f06eed40649d82d2ee
Jangyeonjun <127903702+Jangyeonjun@users.noreply.github.com>
2024-05-30 16:03:39 +0900

    Update script.js

--- 학교 홈패이지 만들기/script.js
+++ 학교 홈패이지 만들기/script.js
@@ -19,3 +19,12 @@ function slide(){
 	$(".imgs>img").eq(now-1).css({"margin-left":"-2000px"});
 	$(".imgs>img").eq(now).css({"margin-left":"0px"});
 }
+
+
+function winOpen1(){
+	var win1 = window.open('login.html','child1','toolbar = no,location = no, staus = no, menubar = no, resizable = no,scrolbars=no, width=700,height = 700')
+}
+
+function winOpen2(){
+	var win2 = window.open('join.html','child2','toolbar = no,location = no, staus = no, menubar = no, resizable = no,scrolbars=no, width=700,height = 700')
+}
